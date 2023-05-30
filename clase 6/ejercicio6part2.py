7ba942977281da7b5eddbb08bcb2d64883ceb666
jagustinrm <jagustin.rm@gmail.com>
2023-05-30 12:58:55 -0300

    ejercicio6part2

---
+++ clase 6/ejercicio6part2.py
@@ -0,0 +1,20 @@
+
+class Alumno:
+    def __init__(self, nombre, nota):
+        self.nombre = nombre
+        self.nota = nota
+
+    def imprimir_informacion(self):
+        print("Nombre:", self.nombre)
+        print("Nota:", self.nota)
+
+    def resultado_aprobacion(self):
+        if self.nota >= 6:
+            print("¡El alumno ha aprobado!")
+        else:
+            print("El alumno ha reprobado.")
+
+
+alumno1 = Alumno("Juan", 7.5)
+alumno1.imprimir_informacion()
+alumno1.resultado_aprobacion()
