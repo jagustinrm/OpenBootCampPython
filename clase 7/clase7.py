90210ad5710ed7eea6fd6b6daf6b1d360117084a
jagustinrm <jagustin.rm@gmail.com>
2023-06-05 15:22:50 -0300

    clase7

---
+++ clase 7/clase7.py
@@ -0,0 +1,13 @@
+import calculadora
+
+resultado_suma = calculadora.sumar(5, 3)
+print(resultado_suma)
+
+resultado_resta = calculadora.restar(8, 2)
+print(resultado_resta)
+
+resultado_multiplicacion = calculadora.multiplicar(4, 6)
+print(resultado_multiplicacion)
+
+resultado_division = calculadora.dividir(10, 2)
+print(resultado_division)
